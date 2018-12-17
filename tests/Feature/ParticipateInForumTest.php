3c99ffd7b92fb4cef7652af6e20942b23fd2ae50
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2018-12-17 21:08:43 +0400

    L6 - l7

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -20,11 +20,11 @@ class ParticipateInForumTest extends TestCase
     /** @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
-        $this->be(factory('App\User')->create());
+        $this->signIn();
 
-        $thread = factory('App\Thread')->create();
-        $reply = factory('App\Reply')->make();
-
+        $thread = create('App\Thread');
+        $reply = make('App\Reply');
+        
         $this->post($thread->path() . '/replies', $reply->toArray());
         $this->get($thread->path())
             ->assertSee($reply->body);
