c1c1d1bcc538a95bff2e5af4631dfaf01f986928
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2018-12-17 17:50:46 +0400

    l6

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CreateThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function guests_may_not_create_new_forum_threads()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+        
+        $this->post('/threads', []);
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_create_new_forum_threads()
+    {
+        $this->actingAs(factory('App\User')->create());
+
+        $thread = factory('App\Thread')->make();
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
