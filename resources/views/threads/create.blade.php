27108df48b6a2c04cc6d5e97e6b4a2d8ace225d2
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2018-12-19 16:38:58 +0400

    L11

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -12,15 +12,37 @@
               {{ csrf_field() }}
               <div class="form-group">
                 <label for="title">Title:</label>
-                <input class="form-control" type="text" id="title" name="title">
+                <input class="form-control" type="text" id="title" name="title" value="{{ old('title') }}" required>
+              </div>
+
+              <div class="form-group">
+                <label for="channel_id">Choose a Channel:</label>
+                <select class="form-control" name="channel_id" id="channel_id" required>
+                  <option value="">Choose One...</option>
+                  @foreach (App\Channel::all() as $channel)
+                    <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>
+                      {{ $channel->name }}
+                    </option>
+                  @endforeach
+                </select>
               </div>
 
               <div class="form-group">
                 <label for="body">Body:</label>
-                <textarea class="form-control"name="body" id="body" rows="8"></textarea>
+                <textarea class="form-control"name="body" id="body" rows="8" value="{{ old('body') }}" required></textarea>
               </div>
 
-              <button class="btn btn-primary" type="submit">Publish</button>
+              <div class="form-group">
+                <button class="btn btn-primary" type="submit">Publish</button>
+              </div>
+
+              @if (count($errors))
+                <ul class="alert alert-danger">
+                  @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                  @endforeach
+                </ul>
+              @endif
             </form>
           </div>
         </div>
