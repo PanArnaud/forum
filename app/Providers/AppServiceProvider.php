88303667f66038b90ff9bf0a718efe1797fa18a0
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2018-12-19 17:02:20 +0400

    L13

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Channel;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \View::share('channels', Channel::all());
     }
 
     /**
@@ -21,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
+
     public function register()
     {
         //
