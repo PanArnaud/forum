d6fc0ccdf905480be11d4acc48b15c4c32a83a14
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2018-12-21 03:04:58 +0400

    'L17'

--- app/Filters/ThreadFilters.php
+++ app/Filters/ThreadFilters.php
@@ -7,7 +7,7 @@ use App\Filters\Filters;
 
 class ThreadFilters extends Filters
 {
-	protected $filters = ['by'];
+	protected $filters = ['by', 'popular'];
 
 	/**
 	 * Filter the query by a given username
@@ -20,5 +20,16 @@ class ThreadFilters extends Filters
 		$user = User::where('name', $username)->firstOrFail();
 
 		return $this->builder->where('user_id', $user->id);
+	}
+
+	/**
+	 * Filter the query acording to most popular threads
+	 * @return $this
+	 */
+	protected function popular()
+	{
+		$this->builder->getQuery()->orders = [];
+		
+		return $this->builder->orderBy('replies_count', 'desc');
 	}
 }
