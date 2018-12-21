bb136b5993a69117ca7c90252dffcc76816fb96e
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2018-12-21 18:02:01 +0400

    'L19'

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -18,6 +18,8 @@ class FavoritesController extends Controller
 
     public function store(Reply $reply)
     {
-        return $reply->favorite();
+        $reply->favorite();
+
+        return back();
     }
 }
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,6 +1,19 @@
 <div class="panel panel-default">
     <div class="panel-heading">
-        <a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }}
+        <div class="level">
+            <h5 class="flex">
+                <a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }}
+            </h5>
+
+            <div>
+                <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                    {{ csrf_field() }}
+                    <button class="btn btn-default" type="submit" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                        {{ $reply->favorites()->count() }} {{ str_plural('Favorite', $reply->favorites()->count()) }}
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
 
     <div class="panel-body">
