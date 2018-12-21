5601e35eab69806e1d874f223fe7d726078164f7
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2018-12-21 13:46:08 +0400

    'L18'

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Reply;
+use App\Favorite;
+use Illuminate\Http\Request;
+
+class FavoritesController extends Controller
+{
+    /**
+     * FavoritesController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(Reply $reply)
+    {
+        return $reply->favorite(auth()->id());
+    }
+}
